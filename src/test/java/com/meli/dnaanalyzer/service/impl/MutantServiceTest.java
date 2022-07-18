220a94469fdcf909b95093efebdc6c3a0da0873a
Edison Quijano <eandresk.13@hotmail.com>
2022-07-17 22:04:10 -0500

    add cloud

--- src/test/java/com/meli/dnaanalyzer/service/impl/MutantServiceTest.java
+++ src/test/java/com/meli/dnaanalyzer/service/impl/MutantServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.stream.Stream;
@@ -19,6 +20,7 @@ import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("dev")
 class MutantServiceTest {
 
     @MockBean
